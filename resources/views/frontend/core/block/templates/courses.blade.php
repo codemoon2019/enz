2bdf729c22fc2c067733cfc2946fc874c146d001
janno <janno.halcyondigital@gmail.com>
2019-04-16 08:58:32 +0800

    courses

--- resources/views/frontend/core/block/templates/courses.blade.php
+++ resources/views/frontend/core/block/templates/courses.blade.php
@@ -2,25 +2,71 @@
     <div class="container-fluid px180">
         <h2 class="title fs40 text-nblue mb50">Choose the Best Courses</h2>   
         <div class="row comm-service">
-            <div class="col-sm-7 left-content">
+            <div class="col-sm-6 left-content padded">
                 <img class="floating plane" src="{{asset('svg/CommunityServiceAirplane.svg')}}" alt="">
                 <img class="floating-rev teacher" src="{{asset('svg/teacher.svg')}}" alt="">
             </div>  
-            <div class="col-sm-5 right-content pl5p align-self-center">
+            <div class="col-sm-6 right-content pl10p align-self-center">
                 <h3 class="title fs40 mb30">Community Services</h3>
                 <p class="basic mb30">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minus. Dolore, eveniet harum quidem modi maxime sequi aliquid dignissimos fugit.</p>
                 <a href="#" class="btn btnview-more text-uppercase">View more</a>
             </div>  
         </div>  
         <div class="row bus-service">
-            <div class="col-sm-5 left-content pr5p align-self-center">
+            <div class="col-sm-6 left-content pr10p align-self-center">
                 <h3 class="title fs40 mb30">Business, Management &amp; Marketing</h3>
                 <p class="basic mb30">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minus. Dolore, eveniet harum quidem modi maxime sequi aliquid dignissimos fugit.</p>
                 <a href="#" class="btn btnview-more text-uppercase">View more</a>
             </div>  
-            <div class="col-sm-7 right-content">
+            <div class="col-sm-6 right-content padded">
                 <img class="floating bman" src="{{asset('svg/bman.svg')}}" alt="">
             </div> 
         </div>  
+        <div class="row hosp-service">
+            <div class="col-sm-6 left-content padded">
+                {{-- <img class="floating plane" src="{{asset('svg/HCAirplane.svg')}}" alt=""> --}}
+                <img class="floating-rev cook" src="{{asset('svg/HospitalityandCookery.svg')}}" alt="">
+                <img class="floating pan" src="{{asset('svg/frying-pan.svg')}}" alt="">
+            </div>  
+            <div class="col-sm-6 right-content pl10p align-self-center">
+                <h3 class="title fs40 mb30">Hospitality and Cookery</h3>
+                <p class="basic mb30">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minus. Dolore, eveniet harum quidem modi maxime sequi aliquid dignissimos fugit.</p>
+                <a href="#" class="btn btnview-more text-uppercase">View more</a>
+            </div>  
+        </div> 
+        <div class="row it-service">
+            <div class="col-sm-6 left-content pr10p align-self-center">
+                <h3 class="title fs40 mb30">Information &amp; Technology</h3>
+                <p class="basic mb30">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minus. Dolore, eveniet harum quidem modi maxime sequi aliquid dignissimos fugit.</p>
+                <a href="#" class="btn btnview-more text-uppercase">View more</a>
+            </div>  
+            <div class="col-sm-6 right-content padded">
+                <img class="floating it" src="{{asset('svg/it.svg')}}" alt="">
+            </div> 
+        </div> 
+        <div class="row nurse-service">
+            <div class="col-sm-6 left-content padded">
+                {{-- <img class="floating plane" src="{{asset('svg/HCAirplane.svg')}}" alt=""> --}}
+                <img class="floating-rev stetos" src="{{asset('svg/stethoscope.svg')}}" alt="">
+                <img class="floating nurse" src="{{asset('svg/nurse.svg')}}" alt="">
+            </div>  
+            <div class="col-sm-6 right-content pl10p align-self-center">
+                <h3 class="title fs40 mb30">Initial Registration for Overseas Nurses</h3>
+                <p class="basic mb30">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minus. Dolore, eveniet harum quidem modi maxime sequi aliquid dignissimos fugit.</p>
+                <a href="#" class="btn btnview-more text-uppercase">View more</a>
+            </div>  
+        </div> 
+        <div class="row other-service">
+            <div class="col-sm-6 left-content pr10p align-self-center">
+                <h3 class="title fs40 mb30">Other Courses</h3>
+                <p class="basic mb30">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minus. Dolore, eveniet harum quidem modi maxime sequi aliquid dignissimos fugit.</p>
+                <a href="#" class="btn btnview-more text-uppercase">View more</a>
+            </div>  
+            <div class="col-sm-6 right-content padded">
+                <img class="floating plane" src="{{asset('svg/CommunityServiceAirplane.svg')}}" alt="">
+                <img class="floating tooth" src="{{asset('svg/tooth.svg')}}" alt="">
+                <img class="floating-rev driller" src="{{asset('svg/OtherCourses.svg')}}" alt="">
+            </div> 
+        </div> 
     </div>
 </div>
