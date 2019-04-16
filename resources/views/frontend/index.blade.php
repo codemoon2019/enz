78a1170ac05e04748111b86d3426d9855b39fcdf
Nico <nico.halcyondigital@gmail.com>
2019-04-16 14:30:41 +0800

    edit home banner content

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,8 +7,12 @@
 	<div class="banner-block banner container-fluid px180 mt20 relative">
         <div class="row">
             <div class="col-sm-5 for-text align-self-center pb10p">
-                <h2 class="title fs40 mb30">Welcome to ENZ Education Consultancy Services</h2>
-                <p class="basic">With the outstanding services and assistance being extended to our clients, ENZ Education Consultancy Services is extremely honored and a proud recipient of two major awards: The National Customers’ Choice Annual Awards for Business Excellence 2016</p>
+
+                {!! findInformation('home-banner-content')->value !!}
+
+{{--                 <h2 class="title fs40 mb30">Welcome to ENZ Education Consultancy Services</h2>
+                <p class="basic">With the outstanding services and assistance being extended to our clients, ENZ Education Consultancy Services is extremely honored and a proud recipient of two major awards: The National Customers’ Choice Annual Awards for Business Excellence 2016</p> --}}
+               
                 <a href="#" class="btn btnread-more text-uppercase">Read more</a>
             </div>
             <div class="col-sm-7 for-video">
