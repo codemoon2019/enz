24b6fdcc6d6dc0c3dbcd5fcc994963b9eede456f
janno <janno.halcyondigital@gmail.com>
2019-04-16 15:31:15 +0800

    footer

--- resources/views/frontend/includes/main-footer.blade.php
+++ resources/views/frontend/includes/main-footer.blade.php
@@ -1,9 +1,47 @@
 <footer id="footer" class="footer" data-anchor="footer">
-    <div class="container credits py24">
+    <section class="questions py80">
+	    <div class="container-fluid relative">
+            <div class="row">
+                <div class="col-sm-3 left-content flags align-self-center">
+                    <img class="img-fluid" src="{{asset('svg/flags.svg')}}" alt="">
+                </div>
+                <div class="col-sm-9 left-content pl10p">
+                    <h2 class="title fs40 text-nblue mb80">Got a Question?</h2>
+                    <div class="form">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                  <label for="">Full Name</label>
+                                  <input type="text" name="" id="" class="form-control" placeholder="" />
+                                </div>
+                                <div class="form-group">
+                                  <label for="">Full Name</label>
+                                  <input type="text" name="" id="" class="form-control" placeholder="" />
+                                </div>
+                                <div class="form-group">
+                                  <label for="">Full Name</label>
+                                  <input type="text" name="" id="" class="form-control" placeholder="" />
+                                </div>
+                                <div class="form-group">
+                                  <label for="">Full Name</label>
+                                  <input type="text" name="" id="" class="form-control" placeholder="" />
+                                </div>
+                                <div class="form-group">
+                                  <label for="">Full Name</label>
+                                  <input type="text" name="" id="" class="form-control" placeholder="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- <div class="container credits py24">
         <ul class="list-unstyled list-inline my0">
             <li class="list-inline-item basic text-black m0">&copy; Copyright 2019 Riviera Villas</li>
             <li class="list-inline-item basic m0"><a class="text-green" href="#">Privacy Policy</a></li>
             <li class="list-inline-item basic m0"><a class="text-green" href="#">Terms of use</a></li>
         </ul>
-    </div>
+    </div> --}}
 </footer>
