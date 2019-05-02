e22d96e63f66fa521518c83b3a619fd24ea40379
janno <janno.halcyondigital@gmail.com>
2019-05-02 09:21:30 +0800

    going mobile index

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -13088,7 +13088,28 @@ $('document').ready(function () {
     autoplay: false,
     slidesToShow: 2,
     slidesToScroll: 1,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
   });
   $('.left.myarrow').click(function () {
     $('.slick-slider .slick-prev').trigger('click');
