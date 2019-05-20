a809ec8b10604998975dcfe8fac7fad9412df61b
Nico <nico.halcyondigital@gmail.com>
2019-05-21 06:57:38 +0800

    course

--- resources/views/frontend/course/index.blade.php
+++ resources/views/frontend/course/index.blade.php
@@ -11,19 +11,29 @@
     <div class="container-fluid px180">
         
         <h2 class="title text-nblue fs40 text-center mb80">Area of Study</h2>
+        
         <div class="row justify-content-center area-of-study">
     
             @foreach (AreaOfStudy() as $key => $study)
         
                 <div class="col-lg-3 col-md-6 item text-center mb60">
+        
                     <div class="card">
+        
                         <img data-src="{{ asset('img/services/newzealand.jpg') }}" class="img-fluid" alt="">
+        
                         <div class="card-footer linear-gradient-teal">
+        
                             <p class="card-title text-uppercase fs18">
+        
                                 <a href="{{ route('frontend.area-of-studies.show', $study->slug) }}" class="btn text-white">{{ $study->title }}</a>
+        
                             </p>
+        
                         </div>
+        
                     </div>        
+        
                 </div>
         
             @endforeach
