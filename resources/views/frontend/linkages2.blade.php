cb95883a777ccd880f51adf035e20b5629a4223e
janno <janno.halcyondigital@gmail.com>
2019-05-29 13:44:55 +0800

    missing pages

--- resources/views/frontend/linkages2.blade.php
+++ resources/views/frontend/linkages2.blade.php
@@ -10,15 +10,24 @@
 <div class="block why-choose-block">
     <div class="container-fluid px475">
         <div class="row">
-            <div class="col-sm-4 item mb30">
+            <div class="col-sm-4 item text-center mb30">
                 <div class="svg-holder mb20 mx-auto">
                     <img alt="" class="img-fluid" src="{{asset('img/awards/award1.png')}}" style="">
                 </div>
+                <p class="basic">National Customers’ Choice Annual Awards for Business Excellence 2016 – Most Outstanding and Quality Education Consultancy Service Provider from the NATIONAL CUSTOMERS CHOICES AWARDS COUNCIL (Insert Logo Award)</p>
             </div>
-        </div>
-        {{-- for news --}}
-        <div class="news-img text-center">
-            <img alt="" class="img-fluid" src="http://www.enz.com.ph/sites/default/files/images/Fotolia_114204877_Subscription_Monthly_M.jpg" style="">
+            <div class="col-sm-4 item text-center mb30">
+                <div class="svg-holder mb20 mx-auto">
+                    <img alt="" class="img-fluid" src="{{asset('img/awards/award2.png')}}" style="">
+                </div>
+                <p class="basic">Gold Seal of Quality 2016 Awardee – Best Education Consultancy Service Provider from the TOP CHOICE CONSUMERS AWARD Council and the NATIONAL CONSUMERS AFFAIRS FOUNDATION</p>
+            </div>
+            <div class="col-sm-4 item text-center mb30">
+                <div class="svg-holder mb20 mx-auto">
+                    <img alt="" class="img-fluid" src="{{asset('img/awards/award3.png')}}" style="">
+                </div>
+                <p class="basic">Netizen's Best Choice Awardee – Netizen's Best Choice Education Consultancy Provider from the Netizen's Choice Magazine & Publishing INC.</p>
+            </div>
         </div>
     </div>
 </div>
