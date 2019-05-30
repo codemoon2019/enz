1512498cc1593a9b2a18844978a7f8de46620b78
janno <janno.halcyondigital@gmail.com>
2019-05-30 09:58:40 +0800

    events

--- resources/views/frontend/event/show.blade.php
+++ resources/views/frontend/event/show.blade.php
@@ -13,6 +13,46 @@
             {!!  $model->description !!}
 
         </div>
+        <div class="card mb30">
+            <div class="card-header linear-gradient-teal">
+                <h2 class="card-title fs18 text-white mb0">Join the event</h2>
+            </div> 
+            <div class="card-body linear-gradient-grey">
+                <form action="" class="form">
+                    <div class="row">
+                        <div class="col-lg-6 form-group">
+                
+                            <label for="">First Name <span class="text-danger">*</span></label>
+            
+                            <input type="text" name="full_name" id="full_name" class="form-control inquiry-field" placeholder="" />
+            
+                        </div>
+                        <div class="col-lg-6 form-group">
+                
+                            <label for="">Last Name <span class="text-danger">*</span></label>
+            
+                            <input type="text" name="full_name" id="full_name" class="form-control inquiry-field" placeholder="" />
+            
+                        </div>
+                        <div class="col-lg-6 form-group">
+                
+                            <label for="">Event Title <span class="text-danger">*</span></label>
+            
+                            <input type="text" name="full_name" id="full_name" class="form-control inquiry-field" placeholder="" />
+            
+                        </div>
+                        <div class="col-lg-6 form-group">
+                
+                            <label for="">Contact Number <span class="text-danger">*</span></label>
+            
+                            <input type="text" name="full_name" id="full_name" class="form-control inquiry-field" placeholder="" />
+            
+                        </div>
+                    </div>
+                    <div class="text-center mt30"><button type="button" class="btn btnview-more text-uppercase inquiry-submit">Submit</button></div>
+                </form>
+            </div>
+        </div>
 
         @include('frontend.includes.templates.index')
 
