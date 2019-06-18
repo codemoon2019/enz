9f7856f696eed38d2a66a5e1471ba6597c783de2
Nico <nico.halcyondigital@gmail.com>
2019-06-18 13:43:09 +0800

    remove news letter before footer

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -40,6 +40,4 @@
 
     @include('frontend.core.block.templates.choose')    
 
-    <div class="grwf2-wrapper wf2-embedded" style="width: 460px; height: 460px; top: 0px; left: 0px; border-radius: 0px; margin: auto;" id="grwf2_21458301_1dh4h"><iframe src="https://app.getresponse.com/site2/enzpromo_2018?u=BPRi5&amp;webforms_id=BZSR5&amp;v=0" width="460" height="460" sandbox="allow-same-origin allow-forms allow-scripts allow-popups allow-top-navigation" scrolling="no" allowtransparency="true" name="webform_BZSR5" style="border: none; height: 460px; width: 460px;"></iframe></div>
-
 @endsection
