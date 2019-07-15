c800e2334f93aefa14003b1b0940559268b020d1
Nico <nico.halcyondigital@gmail.com>
2019-07-15 09:23:27 +0800

    edit link footer

--- resources/views/frontend/mail/links.blade.php
+++ resources/views/frontend/mail/links.blade.php
@@ -2,7 +2,7 @@ Follow us on social media!
 
 <ul>
 
-	<li><a href="https://www.facebook.com/enzecs/" style="text-decoration: none;"> <img style="width: 14px;" src="{{ asset('svg/facebook.svg') }}" alt=""> Facebook</a></li>
+	<li><a href="https://www.facebook.com/enzecs/" style="text-decoration: none;"> <img style="width: 14px;" src="https://enz.com.ph/svg/facebook.svg" alt=""> Facebook</a></li>
 
 	<li><a href="https://instagram.com/enzconsultancy" style="text-decoration: none;"> <img style="width: 14px;" src="{{ asset('svg/instagram.svg') }}" alt=""> Instagram</a></li>
 
