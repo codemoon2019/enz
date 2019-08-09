0886294fb75a4b269f716be252317b88978f1190
Nel Mallillin <junel.halcyondigital@gmail.com>
2019-08-09 13:53:56 +0800

    Remove carerrs route
    Signed-off-by: Nel Mallillin <junel.halcyondigital@gmail.com>

--- routes/frontend/career.php
+++ routes/frontend/career.php
@@ -3,11 +3,11 @@
 /**
  * All route names are prefixed with 'frontend.career'.
  */
-Route::group([
-    'namespace' => 'Career',
-    'prefix' => 'careers',
-    'as' => 'careers.',
-], function () {
-    Route::get('', 'CareerController@index')->name('index');
-    Route::get('/{career}', 'CareerController@show')->where('career', '.+')->name('show');
-});
+// Route::group([
+//     'namespace' => 'Career',
+//     'prefix' => 'careers',
+//     'as' => 'careers.',
+// ], function () {
+//     Route::get('', 'CareerController@index')->name('index');
+//     Route::get('/{career}', 'CareerController@show')->where('career', '.+')->name('show');
+// });
