bccb2937be5a20fef7140bafa4c3588ad28169b4
Nico <nico.halcyondigital@gmail.com>
2019-08-20 15:50:08 +0800

    sort backend inquiries

--- resources/views/backend/core/inquiry/index.blade.php
+++ resources/views/backend/core/inquiry/index.blade.php
@@ -8,6 +8,7 @@
         @slot('title', 'Inquiry List')
         @slot('secondary_title', 'Inquiry Management')
         @slot('headers')
+            <td>ID</td>
             <td>Fullname</td>
             <td>Profession</td>
             <td>Email Address</td>
@@ -18,6 +19,7 @@
             <td>Action</td>
         @endslot
         @slot('columns', json_encode([
+            [ 'data' => 'id' ],
             [ 'data' => 'full_name' ],
             [ 'data' => 'profession' ],
             [ 'data' => 'email_address' ],
@@ -28,7 +30,7 @@
         	[ 'data' => 'actions',  'sortable' => false, 'searchable' => false ],
         ]))
         @slot('options', json_encode([
-        	'order' => [ [1, 'desc'] ] 
+        	'order' => [ [0, 'desc'] ] 
         ]))
     @endcomponent
 @endsection
