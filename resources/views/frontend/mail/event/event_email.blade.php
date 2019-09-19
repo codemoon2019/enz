abbf3c45a341ce9071e7702868815418a5c71ab8
Nico <nico.halcyondigital@gmail.com>
2019-09-19 11:32:15 +0800

    edit email link footer social

--- resources/views/frontend/mail/event/event_email.blade.php
+++ resources/views/frontend/mail/event/event_email.blade.php
@@ -44,7 +44,7 @@ Get more updates by following our social media channels!
 	<li><a href="https://www.facebook.com/enzecs/">https://www.facebook.com/enzecs/</a></li>
 	<li><a href="https://instagram.com/enzconsultancy">https://instagram.com/enzconsultancy</a></li>
 	<li><a href="http://twitter.com/enzconsultancy">http://twitter.com/enzconsultancy</a></li>
-	<li><a href="https://www.youtube.com/channel/UCUtB21CGUfJShRJJUYJ4RnQ">Youtube</a></li>
+	<li><a href="https://www.youtube.com/channel/UCUtB21CGUfJShRJJUYJ4RnQ">https://www.youtube.com/channel/UCUtB21CGUfJShRJJUYJ4RnQ</a></li>
 </ul>
 
 Thank you and see you there!
--- resources/views/frontend/mail/links.blade.php
+++ resources/views/frontend/mail/links.blade.php
@@ -2,13 +2,13 @@ Follow us on social media!
 
 <ul>
 
-<li><a href="https://www.facebook.com/enzecs/">Facebook</a></li>
+<li><a href="https://www.facebook.com/enzecs/">https://www.facebook.com/enzecs/</a></li>
 
-<li><a href="https://instagram.com/enzconsultancy">Instagram</a></li>
+<li><a href="https://instagram.com/enzconsultancy">https://instagram.com/enzconsultancy</a></li>
 
-<li><a href="http://twitter.com/enzconsultancy">Twitter</a></li>
+<li><a href="http://twitter.com/enzconsultancy">http://twitter.com/enzconsultancy</a></li>
 
-<li><a href="https://www.youtube.com/channel/UCUtB21CGUfJShRJJUYJ4RnQ">Youtube</a></li>
+<li><a href="https://www.youtube.com/channel/UCUtB21CGUfJShRJJUYJ4RnQ">https://www.youtube.com/channel/UCUtB21CGUfJShRJJUYJ4RnQ</a></li>
 
 </ul>
 
