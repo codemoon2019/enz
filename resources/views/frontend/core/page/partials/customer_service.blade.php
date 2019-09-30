510a8b7cbf571759ba297a5e03cdc7691f6769fd
Nico <nico.halcyondigital@gmail.com>
2019-09-30 09:25:23 +0800

    edit awards

--- resources/views/frontend/core/page/partials/customer_service.blade.php
+++ resources/views/frontend/core/page/partials/customer_service.blade.php
@@ -5,16 +5,3 @@
     @include('frontend.includes.templates.index')
 
 </div>
-{{-- 
-<div class="container-fluid px180 pt50">
-    <h1 class="title fs40 text-center">Customer Service</h1>
-    <p class="basic text-black">ENZ Education Consultancy Services gives priority in the development of the best customer service. Our staffs are trained with good communication and marketing skills. We always believe that Customer Service plays a vital role in customer retention and in account growth by developing trust with customers over problem issues.</p>
-    <p class="basic text-black">Our consultancy firm is guided by the Basic Customer Service Principles:</p>
-    <ul>
-        <li>We build a good customer relationship.</li>
-        <li>We invest on our staff.</li>
-        <li>We listen to complaints.</li>
-        <li>We know our Services.</li>
-        <li>We smile.</li>
-    </ul>
-</div> --}}
