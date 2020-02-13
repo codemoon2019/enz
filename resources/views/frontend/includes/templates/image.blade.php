3206b832a214a4f87bac59546c256fb469422e35
Nico <nico.halcyondigital@gmail.com>
2020-02-13 14:07:18 +0800

    display all images - template - front

--- resources/views/frontend/includes/templates/image.blade.php
+++ resources/views/frontend/includes/templates/image.blade.php
@@ -1,8 +1,19 @@
+{{-- 
 <div class="text-center mb30">
 
     <img alt="" class="img-fluid" src="{{ asset($content->getFirstMediaUrl('images')) }}" style="">
 
-</div>
+</div> --}}
+
+@foreach ($content->getUploaderImages() as $image)
+
+	<div class="text-center mb30">
+
+	    <img alt="" class="img-fluid" src="{{ asset($image->source) }}" style="">
+
+	</div>
+
+@endforeach
 
 @if ($content->body != null)
 
